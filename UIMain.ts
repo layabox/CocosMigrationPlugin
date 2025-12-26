849d8598a865a43aa644e6b82960f1c325a60969
zringhost11 <38626955@qq.com>
2025-12-26 16:08:29 +0800

    更新菜单

--- UIMain.ts
+++ UIMain.ts
@@ -12,28 +12,28 @@ export class UIMain {
     static async importAssetsTest() {
         //let sourceFolder = "C:/Users/WIN11/CocosEmpty3D/assets";
         //let sourceFolder = "C:/Users/WIN11/CocosEmpty3D2/assets";
+        let sourceFolder = "C:/Users/WIN11/CocosHelloWorld/assets"
+        //let sourceFolder = "C:/Users/WIN11/Documents/allProject/cocosProject/DebugTest/assets";
         //let sourceFolder = "C:/Users/WIN11/Documents/allProject/cocosProject/empty3d/NewProject/assets";
-        let sourceFolder = "C:/Users/WIN11/Documents/allProject/cocosProject/ui/assets";
-
+        //let sourceFolder = "C:/Users/WIN11/Documents/allProject/cocosProject/ui/assets";
         //let sourceFolder = "C:/Users/WIN11/CocosHelloWorld/assets";
         let targetFolder = Editor.assetsPath + "/cc-assets/";
-
         Editor.scene.runScript("CocosImportMain.doImport", sourceFolder, targetFolder);
     }
 
-    // @IEditor.menu("App/迁移Cocos/迁移Cocos项目资源")
-    // static async importAssets() {
-    //     let ret = await Editor.showOpenFolderDialog("选择Cocos资源目录");
-    //     if (ret.canceled)
-    //         return;
+    @IEditor.menu("App/迁移Cocos/迁移Cocos项目资源")
+    static async importAssets() {
+        let ret = await Editor.showOpenFolderDialog("选择Cocos资源目录");
+        if (ret.canceled)
+            return;
 
-    //     let sourceFolder = ret.filePaths[0];
+        let sourceFolder = ret.filePaths[0];
 
-    //     ret = await Editor.showOpenAssetsFolderDialog("选择Laya项目资源目录");
-    //     if (ret.canceled)
-    //         return;
+        ret = await Editor.showOpenAssetsFolderDialog("选择Laya项目资源目录");
+        if (ret.canceled)
+            return;
 
-    //     let targetFolder = Editor.assetsPath + "/" + ret.filePaths[0];
-    //     Editor.scene.runScript("CocosImportMain.doImport", sourceFolder, targetFolder);
-    // }
+        let targetFolder = Editor.assetsPath + "/" + ret.filePaths[0];
+        Editor.scene.runScript("CocosImportMain.doImport", sourceFolder, targetFolder);
+    }
 }
