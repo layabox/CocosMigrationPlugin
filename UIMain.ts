5a992fc7cbfaf7743e5e0934117a01158c2077b8
guzhu <guzhu@fairygui.com>
2025-12-29 19:37:21 +0800

    新增README

--- UIMain.ts
+++ UIMain.ts
@@ -1,22 +1,13 @@
 export class UIMain {
-    @IEditor.menu("App/迁移Cocos/迁移Cocos项目资源（测试）")
-    static async importAssetsTest() {
-let sourceFolder = "C:/Users/WIN11/CocosHelloWorld/assets";
-        //let sourceFolder = "C:/Users/WIN11/CocosEmpty3D/assets";
-        let targetFolder = Editor.assetsPath + "/cc-assets/";
-
-        Editor.scene.runScript("CocosImportMain.doImport", sourceFolder, targetFolder);
-    }
-
     @IEditor.menu("App/迁移Cocos/迁移Cocos项目资源")
     static async importAssets() {
-        let ret = await Editor.showOpenFolderDialog("选择Cocos资源目录");
+        let ret = await Editor.showOpenFolderDialog("选择源Cocos资源目录");
         if (ret.canceled)
             return;
 
         let sourceFolder = ret.filePaths[0];
 
-        ret = await Editor.showOpenAssetsFolderDialog("选择Laya项目资源目录");
+        ret = await Editor.showOpenAssetsFolderDialog("选择目标Laya资源目录");
         if (ret.canceled)
             return;
 
